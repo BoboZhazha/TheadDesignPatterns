2d71c8e2738c8112e120e9fa5f3da6fa48cad4a8
zhang@mac <909319019@qq.com>
2019-03-22 10:39:32 +0800

    add project construction

--- Guarded_Suspension/src/Main.java
+++ Guarded_Suspension/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("asdfasdf");
+        System.out.println("Hello guarded suspension");
     }
 }
--- Producer-Consumer/src/Main.java
+++ Producer-Consumer/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("asdfasdfasdf");
+        System.out.println("Hello Producer-Consumer");
     }
 }
